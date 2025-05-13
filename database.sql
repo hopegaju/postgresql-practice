05bfcdfeafaa9faed0c544543c0b67a7441d696c
hopegaju <hopegajupr@gmail.com>
2025-05-13 09:18:45 +0200

    Added Orders and Books table and updated Customers table

--- database.sql
+++ database.sql
@@ -1,7 +1,27 @@
-CREATE TABLE Customer(
-                         customer_id SERIAL PRIMARY KEY,
-                         first_name VARCHAR(100),
-                         last_name VARCHAR(100),
-                         email VARCHAR(150),
-                         phone VARCHAR(50)
+
+--Books table
+CREATE TABLE Books (
+                       book_id SERIAL PRIMARY KEY,
+                       title VARCHAR(255),
+                       author VARCHAR(255),
+                       genre VARCHAR(100),
+                       price DECIMAL(10, 2),
+                       stock_quantity INT
 );
+--customers table
+CREATE TABLE Customers (
+                           customer_id SERIAL PRIMARY KEY,
+                           first_name VARCHAR(100),
+                           last_name VARCHAR(100),
+                           email VARCHAR(150),
+                           phone VARCHAR(50)
+);
+
+-- Orders table
+CREATE TABLE Orders (
+                        order_id SERIAL PRIMARY KEY,
+                        customer_id INT,
+                        order_date DATE,
+                        total_amount DECIMAL(10, 2)
+);
+
